fec84658f12c7ace1b742e0837c515e4b96bb806
linasmk <linas.mackonis@gmail.com>
2018-05-17 00:15:18 +0200

    Added font-awesome cdn

--- includes/header.php
+++ includes/header.php
@@ -5,5 +5,17 @@
 			<span></span>
 			<span></span>
 		</div>
+		<div class="sidebar">
+			<a href="#"><img class="sidebar__img" src="img/logo.jpg" alt="logo"></a>
+			<ul class="sidebar__menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Artists</a></li>
+			</ul>
+			<ul class="sidebar__socialmedia">
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+			</ul>
+		</div>
 		
 	</header>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Art of Ink</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <?php require "includes/header.php" ?>
