9df8ee1af1f50f85660cfd95c85a5c09899f47b5
linasmk <linas.mackonis@gmail.com>
2018-05-16 19:20:24 +0200

    Created includes/header.php

---
+++ includes/header.php
@@ -0,0 +1,5 @@
+<body>
+	<header class="header">
+		<h1>Art of Ink</h1>
+		<img src="img/logo.png" alt="Logo">
+	</header>
--- index.php
+++ index.php
@@ -5,11 +5,7 @@
 	<title>Document</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-	<header class="header">
-		<h1>Art of Ink</h1>
-		<img src="img/logo.png" alt="Logo">
-	</header>
+<?php require "includes/header.php" ?>
 	
 </body>
 </html>
