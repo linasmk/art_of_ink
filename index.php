f336802e3125f29f43be9270f9bdb00384ebdbbe
linasmk <linas.mackonis@gmail.com>
2018-05-16 19:17:11 +0200

    Changed the stylesheets structure again

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<header class="header">
 		<h1>Art of Ink</h1>
+		<img src="img/logo.png" alt="Logo">
 	</header>
 	
 </body>
