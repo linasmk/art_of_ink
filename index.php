11fcd2bc9c17717480751a9e9708cbced5dfe486
linasmk <linas.mackonis@gmail.com>
2018-05-17 02:23:47 +0200

    Finished hamburger menu
    Responsiveness not adjusted yet.

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 
 	<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 
+	<script src="js/hamburger.js"></script>
+
 </head>
 
 <?php require "includes/header.php" ?>
