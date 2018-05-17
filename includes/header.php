4ee6c55d4c4d50f5efec6367299c8d868b564366
linasmk <linas.mackonis@gmail.com>
2018-05-17 12:36:37 +0200

    Hamburger menu finished

--- includes/header.php
+++ includes/header.php
@@ -6,15 +6,15 @@
 			<span></span>
 		</div>
 		<div class="sidebar">
-			<a href="#"><img class="sidebar__img" src="img/logo.jpg" alt="logo"></a>
+			<a href="index.php"><img class="sidebar__img" src="img/logo.png" alt="logo"></a>
 			<ul class="sidebar__menu">
-				<li><a href="#">Home</a></li>
+				<li><a href="index.php">Home</a></li>
 				<li><a href="#">About</a></li>
 				<li><a href="#">Artists</a></li>
 			</ul>
 			<ul class="sidebar__socialmedia">
-				<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-				<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+				<li><a href="https://www.facebook.com/aoiaarhus/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+				<li><a href="https://www.instagram.com/artofink_dk/" target="_blank"><i class="fab fa-instagram"></i></a></li>
 			</ul>
 		</div>
 		
