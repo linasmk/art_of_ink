66c6d7ee8a68bcdcea0e35519d9cb8799ef116f7
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-05-18 11:03:19 +0200

    parallaxexample

---
+++ includes/mainpage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+    height: 100%;
+}
+
+.parallax {
+    /* The image used */
+    background-image: url('artofink.jpg');
+
+    /* Full height */
+    height: 100%; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+</head>
+<body>
+
+<div class="parallax"></div>
+
+<div style="height:1000px;background-color:grey;font-size:36px">
+Parallax
+</div>
+
+<div class="parallax"></div>
+
+</body>
+</html>
