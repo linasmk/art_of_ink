61cc647f42a1d4f1b0c914c6429c34a2200eb431
maciekwoz <33222646+dobish@users.noreply.github.com>
2018-05-19 23:38:38 +0200

    ++

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <script src="js/hamburger.js"></script>
 <script src="js/facebook_like.js"></script>
 <script src="js/date.js"></script>
-<script src="js/instagram_feed.js"></script>
+
 
 </body>
 </html>
