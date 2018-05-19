8e33cf7aef8ac260a7c0af8533eb0e878babccad
linasmk <linas.mackonis@gmail.com>
2018-05-19 17:33:03 +0200

    Styling artists.php buttons

--- artists.php
+++ artists.php
@@ -8,19 +8,19 @@
 					<a href="https://www.instagram.com/jonaswilly/">Jonas Willy Brund-Andersen</a>
 					<img src="img/jwilly.jpg" alt="Jonas Willy">
 					<p>Tattoo artist. Art Of Ink - Aarhus/Denmark Husband and father. Train insane. I tattoo in every style. -No challenge too big, no tattoo too small.</p>
-					<button>See my profile</button>
+					<button><span>See my profile</span></button>
 				</li>
 				<li class="artist-no2">
 					<a href="https://www.instagram.com/simonbrandt8k/">Simon Brandt</a>
 					<img src="img/sbrandt.jpg" alt="Simon Brandt">
 					<p>Proin tellus magna, ornare nec mauris sit amet, interdum lobortis ipsum. Integer mi nibh, iaculis sit amet laoreet ac, tincidunt ac leo. Nunc id neque non urna viverra convallis eget ac mi.</p>
-					<button>See my profile</button>
+					<button><span>See my profile</span></button>
 				</li>
 				<li class="artist-no3">
 					<a href="https://www.instagram.com/kamilla.ri/">Kamilla Rieder</a>
 					<img src="img/krieder.jpg" alt="Kamilla Rieder">
 					<p>Donec ut ultricies erat, et efficitur mi. Vivamus pretium dolor dignissim, viverra orci in, porttitor urna. Praesent ullamcorper scelerisque leo ac mollis.</p>
-					<button>See my profile</button>
+					<button><span>See my profile</span></button>
 				</li>
 			</ul>
 		</article>
