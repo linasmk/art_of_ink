e46a7efbb3e0dad1f581f913c5b82c6a5b4b0a04
linasmk <linas.mackonis@gmail.com>
2018-05-19 14:00:21 +0200

    Added ulstyling mixin, artists_ul nesting, continuing with artists subpage

--- artists.php
+++ artists.php
@@ -3,9 +3,26 @@
 	<main class="main--artists">
 		<article class="article--artists">
 			<h1>Meet Our Artists</h1>
-			<section class="artist-no1"></section>
-			<section class="artist-no2"></section>
-			<section class="artist-no3"></section>
+			<ul class="artists__ul">
+				<li class="artist-no1">
+					<a href="https://www.instagram.com/jonaswilly/">Jonas Willy Brund-Andersen</a>
+					<img src="img/jwilly.jpg" alt="Jonas Willy">
+					<p>Tattoo artist. Art Of Ink - Aarhus/Denmark Husband and father. Train insane. I tattoo in every style. -No challenge too big, no tattoo too small.</p>
+					<button>See my profile</button>
+				</li>
+				<li class="artist-no2">
+					<a href="https://www.instagram.com/simonbrandt8k/">Simon Brandt</a>
+					<img src="img/sbrandt.jpg" alt="Simon Brandt">
+					<p>Proin tellus magna, ornare nec mauris sit amet, interdum lobortis ipsum. Integer mi nibh, iaculis sit amet laoreet ac, tincidunt ac leo. Nunc id neque non urna viverra convallis eget ac mi.</p>
+					<button>See my profile</button>
+				</li>
+				<li class="artist-no3">
+					<a href="https://www.instagram.com/kamilla.ri/">Kamilla Rieder</a>
+					<img src="img/krieder.jpg" alt="Kamilla Rieder">
+					<p>Donec ut ultricies erat, et efficitur mi. Vivamus pretium dolor dignissim, viverra orci in, porttitor urna. Praesent ullamcorper scelerisque leo ac mollis.</p>
+					<button>See my profile</button>
+				</li>
+			</ul>
 		</article>
 	
 	</main>
