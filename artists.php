fbf7215ffc47a53e1e65f2d3940fb7f272b7cf0e
linasmk <linas.mackonis@gmail.com>
2018-05-20 12:54:56 +0200

    Created php file for the artist

--- artists.php
+++ artists.php
@@ -4,19 +4,19 @@
 		<article class="article--artists">
 			<h1>Meet Our Artists</h1>
 			<ul class="artists__ul">
-				<li class="artist-no1">
+				<li>
 					<a href="https://www.instagram.com/jonaswilly/">Jonas Willy Brund-Andersen</a>
 					<img src="img/jwilly.jpg" alt="Jonas Willy">
 					<p>Tattoo artist. Art Of Ink - Aarhus/Denmark Husband and father. Train insane. I tattoo in every style. -No challenge too big, no tattoo too small.</p>
 					<button><span>See my profile</span></button>
 				</li>
-				<li class="artist-no2">
+				<li>
 					<a href="https://www.instagram.com/simonbrandt8k/">Simon Brandt</a>
 					<img src="img/sbrandt.jpg" alt="Simon Brandt">
 					<p>Proin tellus magna, ornare nec mauris sit amet, interdum lobortis ipsum. Integer mi nibh, iaculis sit amet laoreet ac, tincidunt ac leo. Nunc id neque non urna viverra convallis eget ac mi.</p>
 					<button><span>See my profile</span></button>
 				</li>
-				<li class="artist-no3">
+				<li>
 					<a href="https://www.instagram.com/kamilla.ri/">Kamilla Rieder</a>
 					<img src="img/krieder.jpg" alt="Kamilla Rieder">
 					<p>Donec ut ultricies erat, et efficitur mi. Vivamus pretium dolor dignissim, viverra orci in, porttitor urna. Praesent ullamcorper scelerisque leo ac mollis.</p>
