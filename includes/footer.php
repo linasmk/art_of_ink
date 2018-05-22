79a8fe224e8a301d078f941771c01d5605e21538
abolzz <abolzzy@gmail.com>
2018-05-22 14:19:21 +0200

    Adjustments

--- includes/footer.php
+++ includes/footer.php
@@ -2,20 +2,20 @@
 	<h1 class="footer__h">Contact</h1>
 
 <section class="footerSection">
-	
-	<h2>Address</h2>
-	<p class="footer__p">Grønnegade 58 <br>
-	Aarhus, Denmark-8000</p>
-	<iframe class="googleMaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.8801398776245!2d10.199747315447441!3d56.159176968044314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464c3f94e993c217%3A0x97ca538b52882bed!2sGr%C3%B8nnegade+58%2C+8000+Aarhus!5e0!3m2!1sen!2sdk!4v1526569551840" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+	<h2 class="footer__h">Phone</h2>
+	<p class="footer__p">
+		<a href="tel:+4566119060" class="phoneLink"><img class="phoneIcon" src="./img/phone-icon.svg" alt="phone icon">+45 66 11 90 60</a>
+	</p>
 
 </section>
 
 <section class="footerSection">
-
-	<h2 class="footer__h">Phone</h2>
-	<p class="footer__p">
-		<a href="tel:+4566119060" class="phoneLink"><img src="./img/phone-icon.svg" alt="phone icon">+45 66 11 90 60</a>
-	</p>
+	
+	<h2 class="footer__h">Address</h2>
+	<address class="footer__p">Grønnegade 58 <br>
+	Aarhus, Denmark-8000</address>
+	<iframe class="googleMaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.8801398776245!2d10.199747315447441!3d56.159176968044314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464c3f94e993c217%3A0x97ca538b52882bed!2sGr%C3%B8nnegade+58%2C+8000+Aarhus!5e0!3m2!1sen!2sdk!4v1526569551840" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 </section>
 
@@ -40,18 +40,41 @@
 <section class="footerSection">
 
 	<h2 class="footer__h">Opening hours</h2>
-	<ul class="openingHours__ul">
-		<li id="monday">Monday: 10 - 18</li>
-		<li id="tuesday">Tuesday: 11 - 19</li>
-		<li id="wednesday">Wednesday: 10 - 18</li>
-		<li id="thursday">Thursday: 10 - 18</li>
-		<li id="friday">Friday: 10 - 18</li>
-		<li id="saturday">Saturday: Closed</li>
-		<li id="sunday">Sunday: Closed</li>
-	</ul>
+	<table class="openingHours">
+		<tr id="monday">
+			<td>Monday:</td>
+			<td>10 - 18</td>
+		</tr>
+		<tr id="tuesday">
+			<td>Tuesday:</td>
+			<td>11 - 19</td>
+		</tr>
+		<tr id="wednesday">
+			<td>Wednesday:</td>
+			<td>10 - 18</td>
+		</tr>
+		<tr id="thursday">
+			<td>Thursday:</td>
+			<td>10 - 18</td>
+		</tr>
+		<tr id="friday">
+			<td>Friday:</td>
+			<td>10 - 18</td>
+		</tr>
+		<tr id="saturday">
+			<td>Saturday:</td>
+			<td>Closed</td>
+		</tr>
+		<tr id="sunday">
+			<td>Sunday:</td>
+			<td>Closed</td>
+		</tr>
+	</table>
 
 </section>
 
-<small>&copy; Copyright Art of Ink, 2018</small>
+<div class="copyright">
+	<small>&copy; Copyright Art of Ink, 2018</small>
+</div>
 
 </footer>
--- includes/mainpage.php
+++ includes/mainpage.php
@@ -15,7 +15,7 @@
 
 <section class="about">
 
-	<h1 class="about-headline">Who are we?</h1>
+	<h1 class="about-headline">Who are we</h1>
 	<p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries. Lorem Ipsum has been the industry's standard.</p>
 
 </section>
