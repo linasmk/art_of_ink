4af4e533af4f9db550301c6919ddb5851b3bfc66
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-05-25 15:54:52 +0200

    content2

--- about.php
+++ about.php
@@ -3,17 +3,18 @@
 	<div class="aboutPage">
 		<h1>About us</h1>
 		<section class="history">
-		<p class="about">HP is the man behind the success. He started his career as an apprentice for Micheal Guldman, and has since then had his own shops in both Kolding and Odense.
-HP has tattooed for about 20 years, and has taught up several talented artists during this time. He has taught up both Jonas and Simon who are now co-owners of the shop in Aarhus. 
-HP is skillful in all styles, but has always had a certain eye for oriental. He loves freehand work, and will gladly create big japanese pieces this way. 
-HP is the sole owner of the shop in Odense and co-owner of the Aarhus shop.
+		<p class="about">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit neque felis, at suscipit elit viverra consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sodales lectus ante, tempus placerat nisi pharetra ut. Nunc at consequat nibh. Aenean vitae magna eu lorem egestas posuere eget quis nunc. Etiam massa nibh, consequat vitae nisl in, pellentesque porta nisi. Phasellus volutpat faucibus tellus euismod tempor. Nullam mollis ultricies velit, nec finibus nisl lacinia ac. Quisque faucibus sapien lobortis est pretium accumsan.
 </p>
 		<img class="aboutimage" src="img/artofink2.jpg" alt="studio">
 	</section>
 
 		<section class="history">
-		<p class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit neque felis, at suscipit elit viverra consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sodales lectus ante, tempus placerat nisi pharetra ut. Nunc at consequat nibh. Aenean vitae magna eu lorem egestas posuere eget quis nunc. Etiam massa nibh, consequat vitae nisl in, pellentesque porta nisi. Phasellus volutpat faucibus tellus euismod tempor. Nullam mollis ultricies velit, nec finibus nisl lacinia ac. Quisque faucibus sapien lobortis est pretium accumsan.</p>
-		<img class="aboutimage" src="img/artofink2.jpg" alt="studio">
+		<p class="about">HP is the man behind the success. He started his career as an apprentice for Micheal Guldman, and has since then had his own shops in both Kolding and Odense.
+HP has tattooed for about 20 years, and has taught up several talented artists during this time. He has taught up both Jonas and Simon who are now co-owners of the shop in Aarhus. 
+HP is skillful in all styles, but has always had a certain eye for oriental. He loves freehand work, and will gladly create big japanese pieces this way. 
+HP is the sole owner of the shop in Odense and co-owner of the Aarhus shop.</p>
+		<img class="aboutimage" src="img/hp.jpg" alt="studio">
 	</section>
 	
 		<div id="tattoosMade">100</div>
