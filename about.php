917aa6ee3056da82c16842a1639458dbdade5d81
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-05-25 13:25:05 +0200

    aboutpage

--- about.php
+++ about.php
@@ -1,8 +1,19 @@
 <?php require "includes/head.php" ?>
 <?php require "includes/header.php" ?>
-	<section class="aboutPage">
-		<div id="tattoosMade">100</div>
+	<div class="aboutPage">
+		<h1>About us</h1>
+		<section class="history">
+		<p class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit neque felis, at suscipit elit viverra consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sodales lectus ante, tempus placerat nisi pharetra ut. Nunc at consequat nibh. Aenean vitae magna eu lorem egestas posuere eget quis nunc. Etiam massa nibh, consequat vitae nisl in, pellentesque porta nisi. Phasellus volutpat faucibus tellus euismod tempor. Nullam mollis ultricies velit, nec finibus nisl lacinia ac. Quisque faucibus sapien lobortis est pretium accumsan.</p>
+		<img class="aboutimage" src="img/artofink2.jpg" alt="studio">
 	</section>
+
+		<section class="history">
+		<p class="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit neque felis, at suscipit elit viverra consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc sodales lectus ante, tempus placerat nisi pharetra ut. Nunc at consequat nibh. Aenean vitae magna eu lorem egestas posuere eget quis nunc. Etiam massa nibh, consequat vitae nisl in, pellentesque porta nisi. Phasellus volutpat faucibus tellus euismod tempor. Nullam mollis ultricies velit, nec finibus nisl lacinia ac. Quisque faucibus sapien lobortis est pretium accumsan.</p>
+		<img class="aboutimage" src="img/artofink2.jpg" alt="studio">
+	</section>
+	
+		<div id="tattoosMade">100</div>
+	</div>
 <?php include "includes/footer.php" ?>
 
 <script src="js/loader.js"></script>
