528c34f0ecf02fac153e4737673f8bbe02b3512f
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-05-25 15:51:49 +0200

    content

--- artists.php
+++ artists.php
@@ -12,20 +12,29 @@
 					<a href="https://www.instagram.com/jonaswilly/">Jonas Willy Brund-Andersen</a>
 					<img src="img/jwilly.jpg" alt="Jonas Willy">
 					<div class="list-wrapper">
-					<p>Tattoo artist. Art Of Ink - Aarhus/Denmark Husband and father. Train insane. I tattoo in every style. -No challenge too big, no tattoo too small.</p>
+					<p>Jonas started his career back in 2009 when HP took him in as an apprentice in his studio in Odense. 
+It didn’t take long before it was decided that another shop would open in Aarhus, and Jonas moved from Odense to take care of the new shop.
+Today he is the co-owner of the Aarhus shop and has tattooed for almost 10 years. He tattoos in all styles and loves a good challenge – big or small, though he has always had a soft spot for traditional.
+</p>
 					<button><span><a href="jwilly.php">See my profile</a></span></button>
 					</div>
 				</li>
 				<li>
 					<a href="https://www.instagram.com/simonbrandt8k/">Simon Brandt</a>
 					<img src="img/sbrandt.jpg" alt="Simon Brandt">
-					<p>Proin tellus magna, ornare nec mauris sit amet, interdum lobortis ipsum. Integer mi nibh, iaculis sit amet laoreet ac, tincidunt ac leo. Nunc id neque non urna viverra convallis eget ac mi.</p>
+					<p>Simon started his career in Aarhus, shortly after the new shop opened in the city. 
+He was taught up by HP, but spent a lot of his day to day life in company with Jonas, while also being a part of the graffiti environment, which can be seen in a lot of his earlier work. He has since then gravitated mainly towards black and grey with fine lines, dotwork, and a sense of the small details.
+</p>
 					<button><span>See my profile</span></button>
 				</li>
 				<li>
 					<a href="https://www.instagram.com/kamilla.ri/">Kamilla Rieder</a>
 					<img src="img/krieder.jpg" alt="Kamilla Rieder">
-					<p>Donec ut ultricies erat, et efficitur mi. Vivamus pretium dolor dignissim, viverra orci in, porttitor urna. Praesent ullamcorper scelerisque leo ac mollis.</p>
+					<p>Kamilla is the youngest member of the group and is currently the only woman in the shop. 
+She came in as an apprentice in the Aarhus shop, and can be found there daily. 
+Kamilla has a fine line and does well with smaller tattoos, but enjoys doing a bit of everything. When drawing she leans towards a darker style, and is always ready to do pieces that ventures into the dark art style.
+
+</p>
 					<button><span>See my profile</span></button>
 				</li>
 			</ul>
