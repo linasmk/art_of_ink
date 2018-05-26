1b32c644c2770753a1089dce471faf9dd93f3e39
linasmk <linas.mackonis@gmail.com>
2018-05-26 13:57:24 +0200

    Created responsive circle image

--- artists.php
+++ artists.php
@@ -9,24 +9,27 @@
 			<h1>Meet Our Artists</h1>
 			<ul class="artists__ul">
 				<li>
-					<a href="https://www.instagram.com/jonaswilly/">Jonas Willy Brund-Andersen</a>
-					<img src="img/jwilly.jpg" alt="Jonas Willy">
+					<h2><a href="https://www.instagram.com/jonaswilly/">Jonas Willy Brund-Andersen</a></h2>
+					<div class="responsive-circle1 bgimg1"></div>
 					<div class="list-wrapper">
 					<p>Tattoo artist. Art Of Ink - Aarhus/Denmark Husband and father. Train insane. I tattoo in every style. -No challenge too big, no tattoo too small.</p>
 					<button><span><a href="jwilly.php">See my profile</a></span></button>
 					</div>
 				</li>
 				<li>
-					<a href="https://www.instagram.com/simonbrandt8k/">Simon Brandt</a>
+					<h2><a href="https://www.instagram.com/simonbrandt8k/">Simon Brandt</a></h2>
 					<img src="img/sbrandt.jpg" alt="Simon Brandt">
-					<p>Proin tellus magna, ornare nec mauris sit amet, interdum lobortis ipsum. Integer mi nibh, iaculis sit amet laoreet ac, tincidunt ac leo. Nunc id neque non urna viverra convallis eget ac mi.</p>
-					<button><span>See my profile</span></button>
+					<div class="list-wrapper">
+						<p>Proin tellus magna, ornare nec mauris sit amet, interdum lobortis ipsum. Integer mi nibh, iaculis sit amet laoreet ac, tincidunt ac leo. Nunc id neque non urna viverra convallis eget ac mi.</p>
+						<button><span>See my profile</span></button>
 				</li>
 				<li>
-					<a href="https://www.instagram.com/kamilla.ri/">Kamilla Rieder</a>
+					<h2><a href="https://www.instagram.com/kamilla.ri/">Kamilla Rieder</a></h2>
 					<img src="img/krieder.jpg" alt="Kamilla Rieder">
-					<p>Donec ut ultricies erat, et efficitur mi. Vivamus pretium dolor dignissim, viverra orci in, porttitor urna. Praesent ullamcorper scelerisque leo ac mollis.</p>
-					<button><span>See my profile</span></button>
+					<div class="list-wrapper">
+						<p>Donec ut ultricies erat, et efficitur mi. Vivamus pretium dolor dignissim, viverra orci in, porttitor urna. Praesent ullamcorper scelerisque leo ac mollis.</p>
+						<button><span>See my profile</span></button>
+				    </div>
 				</li>
 			</ul>
 		</article>
