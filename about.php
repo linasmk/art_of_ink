ad658e456e0932697be8518f8d139bd1efca9966
abolzz <abolzzy@gmail.com>
2018-05-28 20:52:09 +0200

    footer adjustment for desktop

--- about.php
+++ about.php
@@ -6,21 +6,20 @@
 <?php require "includes/header.php" ?>
 		
 		<section>
-			<h1 class="aboutUs__about-headline">About us</h1>
+			<h1 class="aboutUs__about-headline black">About us</h1>
 
-			<div class="about aboutUs__about parallax intro">
-				<p class="intro-text">
-					We are professional tattoo artists located in 2 beautiful cities - Odense and Aarhus.
-					Art of Ink was born back in the 2008 when the first studio in Odense opened.
-				</p>
-			</div>
-
-			<div class="about aboutUs__about tattooCount white">
+			<div class="about aboutUs__about white">
 				<p>
-					During the years we have established high quality standards and we are not settling down for anything less than greatness in our work. We have made <span id="tattoosMade"></span> tattoos.
+					We are professional tattoo artists with high quality standards and we are not settling down for anything less than greatness in our work. We have made <span id="tattoosMade"></span> tattoos.
 				</p>
 				<a class="call-to-action black" target="_blank" href="http://m.me/aoiaarhus">Want to have one too?</a>
 			</div>
+
+			<div class="parallax intro"></div>
+			<p class="about aboutUs__about black">
+					Our tattoo shops are located in 2 beautiful cities - Odense and Aarhus.
+					Art of Ink was born back in the 2008 when the first studio in Odense opened.
+				</p>
 		</section>
 
 		<div class="parallax diplomas"></div>
@@ -32,7 +31,7 @@
 		</p>
 		<img class="aboutUs__pic hp-pic" src="img/hp-desktop.jpg" alt="HP">
 		<img class="aboutUs__pic hp-pic hp-pic2" src="img/hp.jpg" alt="HP">
-		<p class="about aboutUs__about aboutHP white">
+		<p class="about aboutUs__about white">
 			HP has tattooed for about 20 years, and has taught up several talented artists during this time. HP is skillful in all styles, but has always had a certain eye for oriental. He loves freehand work, and will gladly create big japanese pieces this way.
 		</p>
 	</section>
--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,23 @@
-<footer class="footer" id="contact">
+<footer class="footer black" id="contact">
 	<h1 class="footer__h">Contact</h1>
 
-<section class="footerSection">
+<section class="footer__section">
 
-	<h2 class="footer__h">Phone</h2>
-	<p class="footer__p">
-		<a href="tel:+4566119060" class="phoneLink"><img class="phoneIcon" src="./img/phone-icon.svg" alt="phone icon">+45 66 11 90 60</a>
-	</p>
+		<div class="addressWrapper white">
+			<h2 class="footer__h">Phone</h2>
+			<p class="footer__p">
+				<a href="tel:+4566119060" class="phoneLink"><img class="phoneIcon" src="./img/phone-icon.svg" alt="phone icon">+45 66 11 90 60</a>
+			</p>
+			<h2 class="footer__h">Address</h2>
+			<address class="footer__p">Grønnegade 58, Aarhus, Denmark-8000</address>
+		</div>
+		
+		<iframe class="googleMaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.8801398776245!2d10.199747315447441!3d56.159176968044314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464c3f94e993c217%3A0x97ca538b52882bed!2sGr%C3%B8nnegade+58%2C+8000+Aarhus!5e0!3m2!1sen!2sdk!4v1526569551840" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 </section>
 
-<section class="footerSection">
-	
-	<h2 class="footer__h">Address</h2>
-	<address class="footer__p">Grønnegade 58 <br>
-	Aarhus, Denmark-8000</address>
-	<iframe class="googleMaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2221.8801398776245!2d10.199747315447441!3d56.159176968044314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464c3f94e993c217%3A0x97ca538b52882bed!2sGr%C3%B8nnegade+58%2C+8000+Aarhus!5e0!3m2!1sen!2sdk!4v1526569551840" frameborder="0" style="border:0" allowfullscreen></iframe>
-	
-</section>
 
-
-<section class="footerSection">
+<section class="footer__section">
 
 	<h2 class="footer__h">Social media</h2>
 	<ul class="footer__socialmedia">
@@ -38,7 +35,7 @@
 
 </section>
 
-<section class="footerSection openingHours__section">
+<section class="footer__section openingHours__section">
 
 	<h2 class="footer__h">Opening hours</h2>
 	<p>We are <span class="bold" id="openStatus"></span></p>
