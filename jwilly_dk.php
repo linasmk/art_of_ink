3e6f7ddf655e8f3810a675f84d1d380377649b34
abolzz <abolzzy@gmail.com>
2018-06-25 09:51:39 +0200

    deleted unnecesary script tags

--- jwilly_dk.php
+++ jwilly_dk.php
@@ -50,14 +50,5 @@ Han tatoverer i alle stilarter og elsker en god udfordring. Stort som småt, med
 			</article>
 	</main>
 
-<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-<script src="js/loader.js"></script>
-<script src="js/hamburger.js"></script>
-<script src="js/facebook_like.js"></script>
-<script src="js/date.js"></script>
-<script src="js/maps.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2ED7FrptqUOzKtwuRS134pYzziGJTtQk&callback=initMap"
-    async defer></script>
-
 <?php include "includes/footer.php" ?>
 
